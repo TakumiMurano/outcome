6aa4b5366933370fe3880975016ef6d7f96af7e7
TakumiMurano <shiirosonya98@gmail.com>
2024-02-20 23:59:23 +0900

    スレッド一覧(ホーム)画面の実装を行いました。また、スレッドテーブル・トピックテーブル・大会テーブルにシーディングを行いました

---
+++ database/seeders/ThreadSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class ThreadSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('threads')->insert([
+                'title' => 'テストスレッド2',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+                'user_id' => 1,
+                'topic_id' => 1,
+                'competition_id' => 1,
+         ]);
+        //
+    }
+}
---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Football掲示板プレミア</h1>
+        <div class="threads">
+            @foreach ($threads as $thread)
+                <div class="thread">
+                    <h2 class="title">{{ $thread->title }}</h2>
+                    <p class="foreignId">{{ $thread->topic_id }} {{ $thread->competition_id }}</p>
+                    <p class="comment">{{ $thread->comment }}</p>
+                </div>
+            @endforeach
+        </div>
+    </body>
+</html>
